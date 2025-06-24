7b30445f67305a9e730ea353e94828731e709a3e
MEHTA RAJ JATIN <158834536+RajMehta1265@users.noreply.github.com>
2025-06-24 15:17:34 +0530

    Commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const dbName = 'passop';
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://password-manager-izgk.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
